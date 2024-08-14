5778aaba1a3f719424eff491b5a0193875dc7bf6
tkodimac <tkmachiwenyika@gmail.com>
2024-08-14 21:22:24 +0000

    Collect sales data for the last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -75,6 +75,23 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales() :
+    """
+    Collects columns of Data from sales worksheet,collecting
+    the last 5 entries for each sandwich and returns the data as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -87,4 +104,6 @@ def main():
 
 
 print("Welcome to Water Masters Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
