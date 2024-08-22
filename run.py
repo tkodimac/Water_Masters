b954430f1ccf42ffd5b95f2e093f105fb41587c9
tkodimac <tkmachiwenyika@gmail.com>
2024-08-22 14:48:05 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -91,6 +91,23 @@ def get_last_5_entries_sales() :
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    print(new_stock_data)
+    
+    return new_stock_data
+
+
 
 def main():
     """
@@ -101,9 +118,11 @@ def main():
     update_worksheet(sales_data,"sales")  # Call the function to update the worksheet
     new_surplus_data = calculate_surplus_data(sales_data)  # Call the surplus calculation function
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+
 
 
 print("Welcome to Water Masters Data Automation")
-# main()
-
-sales_columns = get_last_5_entries_sales()
+main()
